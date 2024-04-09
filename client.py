50cdacaabbc103f25dea57257c40f525e3c511c2
Vincent Taffertshofer <vincent.taffertshofer@gmail.com>
2024-04-09 23:20:30 +0200

    redisgn and logs

--- client.py
+++ client.py
@@ -1,11 +1,26 @@
+import os
 import socket
+import struct
 
-def start_client():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(('localhost', 12345))  
-    client.sendall(b"Hallo Server!")  
-    data = client.recv(1024)  
-    print("Antwort vom Server erhalten: ", data.decode())
-    client.close()
 
-start_client()
+def send_file(sck: socket.socket, filename):
+    # Get the size of the outgoing file.
+    filesize = os.path.getsize(filename)
+    print(filesize) # 25 bytes
+    # First inform the server the amount of
+    # bytes that will be sent.
+    sck.sendall(struct.pack("<Q", filesize))
+    print(struct.pack("<Q", filesize)) # b'\x19\x00\x00\x00\x00\x00\x00\x00'
+    # Send the file in 1024-bytes chunks.
+    with open(filename, "rb") as f:
+        while read_bytes := f.read(1024):
+            sck.sendall(read_bytes)
+
+
+with socket.create_connection(("localhost", 6190)) as conn:
+    print("Connected to the server.")
+    print("Sending file...")
+    send_file(conn, "message.txt")
+    print("Sent.")
+
+print("Connection closed.")
--- server.py
+++ server.py
@@ -1,17 +1,55 @@
 import socket
+import struct
 
-def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(('localhost', 12345))
-    server.listen(1) 
-    print("Server gestartet und bereit für Verbindungen.")
 
-    while True:
-        conn, addr = server.accept() 
-        print(f"Verbindung von {addr} hergestellt.")
-        data = conn.recv(1024)
-        print("Nachricht vom Client erhalten: ", data.decode())
-        conn.sendall(b"Meldung erhalten.")
-        conn.close()
+def receive_file_size(sck: socket.socket):
+    # This funcion makes sure that the bytes which indicate
+    # the size of the file that will be sent are received.
+    # The file is packed by the client via struct.pack(),
+    # a function that generates a bytes sequence that
+    # represents the file size.
+    fmt = "<Q"
+    expected_bytes = struct.calcsize(fmt)
+    print(fmt, expected_bytes) # <Q 8
+    received_bytes = 0
+    stream = bytes()
+    while received_bytes < expected_bytes:
+        chunk = sck.recv(expected_bytes - received_bytes)
+        stream += chunk
+        received_bytes += len(chunk)
+        print(chunk, stream, received_bytes) # b'\x19\x00\x00\x00\x00\x00\x00\x00' b'\x19\x00\x00\x00\x00\x00\x00\x00' 8
+    filesize = struct.unpack(fmt, stream)[0]
+    print(filesize) # 25 bytes
+    return filesize
 
-start_server()
+
+def receive_file(sck: socket.socket, filename):
+    # First read from the socket the amount of
+    # bytes that will be received from the file.
+    filesize = receive_file_size(sck)
+
+    # Open a new file where to store the received data.
+    with open(filename, "wb") as f:
+        received_bytes = 0
+        # Receive the file data in 1024-bytes chunks
+        # until reaching the total amount of bytes
+        # that was informed by the client.
+        while received_bytes < filesize:
+            chunk = sck.recv(1024)
+            if chunk:
+                # print(chunk) # b'This is very confidential' (bytes literal, each character is a 8 bit value of UTF-8 or ASCII symbol)
+                f.write(chunk)
+                received_bytes += len(chunk)
+            print(chunk, received_bytes, filesize)
+
+
+with socket.create_server(("localhost", 6190)) as server:
+    while 1:
+        print("Waiting for the client...")
+        conn, address = server.accept()
+        print(f"{address[0]}:{address[1]} connected.")
+        print("Receiving file...")
+        receive_file(conn, "message-received.txt")
+        print("File received.")
+
+print("Connection closed.")
